e54ef06c34a2ff67ea2f12a38df09842e9b4b3f2
jiangjun25372 <jiangjun25372@hundsun.com>
2019-07-13 00:36:30 +0800

    [test]根据年月生成月历 有待完善 生成word

---
+++ monthlycalendar-demo/src/main/java/com/burton/monthlycalendardemo/MonthlycalendarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.burton.monthlycalendardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlycalendarDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonthlycalendarDemoApplication.class, args);
+    }
+
+}
---
+++ monthlycalendar-demo/src/test/java/com/burton/monthlycalendardemo/MonthlycalendarDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.burton.monthlycalendardemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonthlycalendarDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
