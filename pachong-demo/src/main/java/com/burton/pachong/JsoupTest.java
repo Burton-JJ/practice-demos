73c0ae3a2ae1d5814cdb266b126a27cebbdc6b1c
jiangjun25372 <jiangjun25372@hundsun.com>
2019-07-18 02:45:27 +0800

    [test]JSOUP爬去中国政府网放假信息 待续正则匹配日期

---
+++ pachong-demo/src/main/java/com/burton/pachong/JsoupTest.java
@@ -0,0 +1,65 @@
+package com.burton.pachong;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Burton
+ * @title: JsoupTest
+ * @projectName practice-demos
+ * @description: jsoup入门演练
+ * @date 2019/7/180:57
+ */
+public class JsoupTest {
+
+    //年份正则
+    private static final Pattern yearPattern = Pattern.compile("[0-9]{4}");
+
+    //2018年12月30日至2019年1月1日放假调休
+    public static void main(String[] args) throws IOException {
+        String fileUrl = null;
+        String year = null;
+        String url = "http://sousuo.gov.cn/s.htm?t=paper&advance=false&n=&timetype=&mintime=&maxtime=&sort=&q=%E8%8A%82%E5%81%87%E6%97%A5";
+        Document doc = null;
+        doc = Jsoup.connect(url).userAgent("Mozilla").get();
+        Elements div = doc.getElementsByClass("result");
+        Elements ul = div.get(0).getElementsByTag("ul");
+        Elements lis = ul.get(0).getElementsByTag("li");
+        for (Element li : lis) {
+            Elements h3s = li.getElementsByClass("res-sub-title");
+            Element a = h3s.get(0).getElementsByTag("a").get(0);
+            //获取网址
+            fileUrl = a.attr("abs:href");
+            System.out.println(fileUrl);
+            //获取文字 todo 正则表达式匹配出年份
+            String characters = a.text();
+            System.out.println(characters);
+            Matcher matcher = yearPattern.matcher(characters);
+            if (matcher.find()) {
+                year = matcher.group();
+                System.out.println(year);
+            } else {
+                System.out.println("未获得年份信息");
+            }
+            if ("2019".equals(year)) {
+                break;
+            }
+
+        }
+        if (fileUrl != null) {
+            System.out.println("获取链接成功");
+            doc = Jsoup.connect(fileUrl).userAgent("Mozilla").get();
+            Elements ps = doc.getElementById("UCAP-CONTENT")
+                    .getElementsByTag("p");
+            for (Element element : ps) {
+                System.out.println(element.text());
+            }
+        }
+    }
+}
